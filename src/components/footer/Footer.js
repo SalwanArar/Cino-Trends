c7c93d560d4b319f2f7b773b9960b548eb6e5273
salwan.arar@gmail.com <salwan.arar@gmail.com>
2023-03-12 03:34:31 -0700

    Footer Updates

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,89 +1,78 @@
 import React from 'react';
-import { makeStyles } from '@mui/material/styles';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Link, Typography } from '@mui/material';
+import { FacebookRounded, LinkedIn, Twitter, YouTube } from '@mui/icons-material';
 
-// const useStyles = makeStyles((theme) => ({
-//   footer: {
-//     backgroundColor: theme.palette.background.paper,
-//     padding: theme.spacing(6),
-//   },
-// }));
 
 function Footer () {
-//   const classes = useStyles();
-
-//   return (
-    // <h1>
-    //     Hello, World!
-    //     This is a footer
-    // </h1>);
   return (
     <footer style={{
         background: '#090F24',
         padding: '4rem'
     }}>
-        <Grid container spacing={4}>
-            <Grid item xs={12} sm={4} sx={{ color:'#FFF' }}>
-                <Typography variant='h4'>CONTACT US</Typography>
-                <Typography variant='h6' color='red'>cinotrends@gmail.com</Typography>
-                <br/>
-                <br/>
+        <Grid container spacing={4} sx={{ color: '#FFF' }}>
+            <Grid item xs={12} sm={4} sx={{border: 'solid', alignItems: 'center'}}>
+                <Typography variant='h4' sx={{textAlign: 'center', border:'solid'}}>
+                    CONTACT US
+                </Typography>
+                <br />
+                <Link
+                href='mailto:cinotrends@gmail.com'
+                underline='hover'
+                sx={{ fontSize: '1.1rem', border: 'solid'}}>
+                    cinotrends@gmail.com
+                </Link>
+                <br />
+                <br />
                 <Typography variant='h4'>OFFICE</Typography>
-                <Typography variant='h6'><a href='tel:00971507355523'>+971 50 735 5523</a></Typography>
+                <br />
+                <Link
+                href='tel:00971507355523'
+                underline='hover'
+                sx={{ fontSize: '1.1rem' }}>
+                    +971 50 735 5523
+                </Link>
             </Grid>
             <Grid item xs={12} sm={4}>
-                <Typography variant='h6'>
+                <Typography variant='h4' sx={{textAlign: 'center'}}>
                     FOLLOW US
                 </Typography>
-                <ul>
-                    <li><a href='#'>Link 1</a></li>
-                    <li><a href='#'>Link 2</a></li>
-                    <li><a href='#'>Link 3</a></li>
-                </ul>
+                <br/>
+                <br/>
+                <Grid
+                sm={12}
+                sx={{textAlign: 'center'}}>
+                    <Link
+                    href='#Facebook'>
+                        <FacebookRounded sx={{ fontSize: '3rem' }}/>
+                    </Link>
+                    <Link
+                    href='#Youtube'>
+                        <YouTube sx={{ fontSize: '3rem' }}/>
+                    </Link>
+                    <Link
+                    href='#Twitter'>
+                        <Twitter sx={{ fontSize: '3rem' }}/>
+                    </Link>
+                    <Link
+                    href='#LinkedIn'>
+                        <LinkedIn sx={{ fontSize: '3rem' }}/>
+                    </Link>
+                </Grid>
             </Grid>
             <Grid item xs={12} sm={4}>
-                <Typography variant='h6'>
+                <Typography variant='h4' sx={{textAlign: 'center'}}>
                     LOCATION
                 </Typography>
-                <ul>
-                    <li><a href='#'>Link 1</a></li>
-                    <li><a href='#'>Link 2</a></li>
-                    <li><a href='#'>Link 3</a></li>
-                </ul>
+                <br/>
+                <Link
+                underline='hover'
+                sx={{ fontSize: '1.1rem' }}>
+                    Cino Trends FZ LLC<br />
+                    Cornich View Tower, Office 101,<br />
+                    Al Markaziyah West, Abu Dhabi, UAE
+                </Link>
             </Grid>
         </Grid>
-      {/* <Grid container spacing={4}>
-        <Grid item xs={12} sm={4}>
-          <Typography variant="h6" gutterBottom>
-            Column 1
-          </Typography>
-          <ul>
-            <li><a href="#">Link 1</a></li>
-            <li><a href="#">Link 2</a></li>
-            <li><a href="#">Link 3</a></li>
-          </ul>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Typography variant="h6" gutterBottom>
-            Column 2
-          </Typography>
-          <ul>
-            <li><a href="#">Link 4</a></li>
-            <li><a href="#">Link 5</a></li>
-            <li><a href="#">Link 6</a></li>
-          </ul>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Typography variant="h6" gutterBottom>
-            Column 3
-          </Typography>
-          <ul>
-            <li><a href="#">Link 7</a></li>
-            <li><a href="#">Link 8</a></li>
-            <li><a href="#">Link 9</a></li>
-          </ul>
-        </Grid>
-      </Grid> */}
     </footer>
   );
 };
